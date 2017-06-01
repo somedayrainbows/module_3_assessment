6b6402e1f763b5fc6c8d6a46306195e26ef67835
somedayrainbows <ebassity@gmail.com>
2017-06-01 12:15:29 -0600

    Final commit :(

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,15 +1,15 @@
 class Search
-  attr_reader :total_stores, :stores
-
-  def initialize(raw_results={})
-    @total_stores = raw_results[:total]
-    @stores = raw_results[:stores]
-  end
+  # attr_reader :total_stores, :stores
+  #
+  # def initialize
+  #   @total_stores = raw_results[:total]
+  #   @stores = raw_results[:stores]
+  # end
 
   def self.get_stores(zipcode)
     service = SearchService.new({zipcode: zipcode})
     raw_results = service.find_stores
+    binding.pry
   end
-
 
 end
